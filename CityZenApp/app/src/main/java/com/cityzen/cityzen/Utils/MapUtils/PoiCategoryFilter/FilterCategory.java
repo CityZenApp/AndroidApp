e6dce84a331e295a7df75d9af43e84f5c61baf3d
AndyScherzinger <info@andy-scherzinger.de>
2018-03-04 14:38:14 +0100

    recolored

--- CityZenApp/app/src/main/java/com/cityzen/cityzen/Utils/MapUtils/PoiCategoryFilter/FilterCategory.java
+++ CityZenApp/app/src/main/java/com/cityzen/cityzen/Utils/MapUtils/PoiCategoryFilter/FilterCategory.java
@@ -9,82 +9,27 @@ import java.util.ArrayList;
 public class FilterCategory {
 
     /**
-     * <!--Points Of interest-->
-     * <array name="poi_titles"> <!--Localization needed-->
-     * <item>Cafés</item>
-     * <item>Bars/clubs</item>
-     * <item>Restaurants</item>
-     * <item>Bakeries</item>
-     * <item>Fast food</item>
-     * <item>Tourism</item>
-     * <item>Hotels/hostels</item>
-     * <item>Transportation</item>
-     * <item>Gas stations</item>
-     * <item>Pharmacies/healthcare</item>
-     * <item>Banks, ATMs &amp; exchange</item>
-     * <item>Mobile phone stores</item>
-     * <item>Markets/kiosks</item>
-     * <item>Shopping malls</item>
-     * <item>Parking</item>
-     * <item>Rent a bicycle</item>
-     * <item>Rent a car &amp; car sharing</item>
-     * </array>
-     * <p>
-     * <array name="poi_id"> <!--Don't localize, used for app functionality-->
-     * <item>0</item>
-     * <item>1</item>
-     * <item>2</item>
-     * <item>3</item>
-     * <item>4</item>
-     * <item>5</item>
-     * <item>6</item>
-     * <item>7</item>
-     * <item>8</item>
-     * <item>9</item>
-     * <item>10</item>
-     * <item>11</item>
-     * <item>12</item>
-     * <item>13</item>
-     * <item>14</item>
-     * <item>15</item>
-     * <item>16</item>
-     * </array>
+     * category_accomodation
+     * category_atm
+     * category_bar
+     * category_cafe
+     * category_fast_food
+     * category_gas_station
+     * category_market
+     * category_mobile_phone_store
+     * category_parking
+     * category_pharmacy
+     * category_rental_bike
+     * category_rental_car
+     * category_restaurant
+     * category_shopping_mall
+     * category_tourism
+     * category_transportation
      */
-
     public static ArrayList<OsmTag> getFilters(int poiId) {
         ArrayList<OsmTag> tags = new ArrayList<>();
         switch (poiId) {
             case 0:
-                tags.add(new OsmTag("amenity", "cafe"));
-                tags.add(new OsmTag("shop", "coffee"));
-                break;
-            case 1:
-                tags.add(new OsmTag("amenity", "bar"));
-                tags.add(new OsmTag("amenity", "nightclub"));
-                tags.add(new OsmTag("amenity", "pub"));
-                break;
-            case 2:
-                tags.add(new OsmTag("amenity", "restaurant"));
-                tags.add(new OsmTag("amenity", "food_court"));
-                break;
-            case 3:
-                tags.add(new OsmTag("shop", "bakery"));
-                tags.add(new OsmTag("shop", "pastry"));
-                tags.add(new OsmTag("shop", "confectionery"));
-                tags.add(new OsmTag("industrial", "bakery"));
-                break;
-            case 4:
-                tags.add(new OsmTag("amenity", "fast_food"));
-                break;
-            case 5:
-                tags.add(new OsmTag("tourism", "museum"));
-                tags.add(new OsmTag("tourism", "information"));
-                tags.add(new OsmTag("tourism", "attraction"));
-                tags.add(new OsmTag("tourism", "yes"));
-                tags.add(new OsmTag("historic", "monument"));
-                tags.add(new OsmTag("tourism", "zoo"));
-                break;
-            case 6:
                 tags.add(new OsmTag("building", "hotel"));
                 tags.add(new OsmTag("tourism", "hotel"));
                 tags.add(new OsmTag("tourism", "apartment"));
@@ -92,18 +37,45 @@ public class FilterCategory {
                 tags.add(new OsmTag("tourism", "motel"));
                 tags.add(new OsmTag("tourism", "guest_house"));
                 break;
-            case 7:
-                tags.add(new OsmTag("amenity", "taxi"));
-                tags.add(new OsmTag("amenity", "bus_station"));
-                tags.add(new OsmTag("railway", "station"));
-                tags.add(new OsmTag("highway", "bus_stop"));
-                tags.add(new OsmTag("railway", "tram_stop"));
-                tags.add(new OsmTag("public_transport", "station"));
-                tags.add(new OsmTag("railway", "subway_entrance"));
+            case 1:
+                tags.add(new OsmTag("amenity", "atm"));
+                tags.add(new OsmTag("amenity", "bank"));
+                tags.add(new OsmTag("amenity", "bureau_de_change"));
                 break;
-            case 8:
+            case 2:
+                tags.add(new OsmTag("amenity", "bar"));
+                tags.add(new OsmTag("amenity", "nightclub"));
+                tags.add(new OsmTag("amenity", "pub"));
+                break;
+            case 3:
+                tags.add(new OsmTag("amenity", "cafe"));
+                tags.add(new OsmTag("shop", "coffee"));
+                break;
+            case 4:
+                tags.add(new OsmTag("amenity", "fast_food"));
+                break;
+            case 5:
                 tags.add(new OsmTag("amenity", "fuel"));
                 tags.add(new OsmTag("shop", "fuel"));
+                break;
+            case 6:
+                tags.add(new OsmTag("amenity", "marketplace"));
+                tags.add(new OsmTag("building", "retail"));
+                tags.add(new OsmTag("building", "kiosks"));
+                tags.add(new OsmTag("shop", "convenience"));
+                tags.add(new OsmTag("shop", "kiosks"));
+                tags.add(new OsmTag("shop", "supermarket"));
+                tags.add(new OsmTag("shop", "general"));
+                tags.add(new OsmTag("shop", "variety_store"));
+                tags.add(new OsmTag("shop", "tabacco"));
+                break;
+            case 7:
+                tags.add(new OsmTag("shop", "mobile_phone"));
+                break;
+            case 8:
+                tags.add(new OsmTag("amenity", "parking"));
+                tags.add(new OsmTag("amenity", "parking_space"));
+                tags.add(new OsmTag("building", "parking"));
                 break;
             case 9:
                 tags.add(new OsmTag("amenity", "clinic"));
@@ -115,39 +87,36 @@ public class FilterCategory {
                 tags.add(new OsmTag("healthcare", "alternative"));
                 break;
             case 10:
-                tags.add(new OsmTag("amenity", "atm"));
-                tags.add(new OsmTag("amenity", "bank"));
-                tags.add(new OsmTag("amenity", "bureau_de_change"));
+                tags.add(new OsmTag("amenity", "bicycle_rental"));
                 break;
             case 11:
-                tags.add(new OsmTag("shop", "mobile_phone"));
+                tags.add(new OsmTag("amenity", "car_rental"));
+                tags.add(new OsmTag("amenity", "car_sharing"));
                 break;
             case 12:
-                tags.add(new OsmTag("amenity", "marketplace"));
-                tags.add(new OsmTag("building", "retail"));
-                tags.add(new OsmTag("building", "kiosks"));
-                tags.add(new OsmTag("shop", "convenience"));
-                tags.add(new OsmTag("shop", "kiosks"));
-                tags.add(new OsmTag("shop", "supermarket"));
-                tags.add(new OsmTag("shop", "general"));
-                tags.add(new OsmTag("shop", "variety_store"));
-                tags.add(new OsmTag("shop", "tabacco"));
+                tags.add(new OsmTag("amenity", "restaurant"));
+                tags.add(new OsmTag("amenity", "food_court"));
                 break;
             case 13:
                 tags.add(new OsmTag("shop", "department_store"));
                 tags.add(new OsmTag("shop", "mall"));
                 break;
             case 14:
-                tags.add(new OsmTag("amenity", "parking"));
-                tags.add(new OsmTag("amenity", "parking_space"));
-                tags.add(new OsmTag("building", "parking"));
+                tags.add(new OsmTag("tourism", "museum"));
+                tags.add(new OsmTag("tourism", "information"));
+                tags.add(new OsmTag("tourism", "attraction"));
+                tags.add(new OsmTag("tourism", "yes"));
+                tags.add(new OsmTag("historic", "monument"));
+                tags.add(new OsmTag("tourism", "zoo"));
                 break;
             case 15:
-                tags.add(new OsmTag("amenity", "bicycle_rental"));
-                break;
-            case 16:
-                tags.add(new OsmTag("amenity", "car_rental"));
-                tags.add(new OsmTag("amenity", "car_sharing"));
+                tags.add(new OsmTag("amenity", "taxi"));
+                tags.add(new OsmTag("amenity", "bus_station"));
+                tags.add(new OsmTag("railway", "station"));
+                tags.add(new OsmTag("highway", "bus_stop"));
+                tags.add(new OsmTag("railway", "tram_stop"));
+                tags.add(new OsmTag("public_transport", "station"));
+                tags.add(new OsmTag("railway", "subway_entrance"));
                 break;
             default:
                 break;
--- CityZenApp/app/src/main/java/com/cityzen/cityzen/Utils/RecyclerView/CategoryColoringUtil.java
+++ CityZenApp/app/src/main/java/com/cityzen/cityzen/Utils/RecyclerView/CategoryColoringUtil.java
@@ -34,7 +34,7 @@ public abstract class CategoryColoringUtil {
                 setupItemIcon(context, R.drawable.ic_local_city, R.color.category_building, categoryImageView);
                 break;
             case "mall":
-                setupItemIcon(context, R.drawable.ic_local_city, R.color.category_mall, categoryImageView);
+                setupItemIcon(context, R.drawable.ic_local_city, R.color.category_shopping_mall, categoryImageView);
                 break;
             case "residential":
                 setupItemIcon(context, R.drawable.ic_local_city, R.color.category_building, categoryImageView);
@@ -52,58 +52,58 @@ public abstract class CategoryColoringUtil {
                 setupItemIcon(context, R.drawable.ic_local_city, R.color.category_building, categoryImageView);
                 break;
             case "hotel":
-                setupItemIcon(context, R.drawable.ic_home_white, R.color.category_hotel, categoryImageView);
+                setupItemIcon(context, R.drawable.ic_home_white, R.color.category_accomodation, categoryImageView);
                 break;
             case "hostel":
-                setupItemIcon(context, R.drawable.ic_home_white, R.color.category_home, categoryImageView);
+                setupItemIcon(context, R.drawable.ic_home_white, R.color.category_accomodation, categoryImageView);
                 break;
             case "guest_house":
-                setupItemIcon(context, R.drawable.ic_home_white, R.color.category_home, categoryImageView);
+                setupItemIcon(context, R.drawable.ic_home_white, R.color.category_accomodation, categoryImageView);
                 break;
             case "attraction":
-                setupItemIcon(context, R.drawable.ic_nature_people_white, R.color.category_attraction, categoryImageView);
+                setupItemIcon(context, R.drawable.ic_nature_people_white, R.color.category_tourism, categoryImageView);
                 break;
             case "pedestrian":
-                setupItemIcon(context, R.drawable.ic_nature_people_white, R.color.category_attraction, categoryImageView);
+                setupItemIcon(context, R.drawable.ic_nature_people_white, R.color.category_tourism, categoryImageView);
                 break;
             case "monument":
-                setupItemIcon(context, R.drawable.ic_nature_people_white, R.color.category_attraction, categoryImageView);
+                setupItemIcon(context, R.drawable.ic_nature_people_white, R.color.category_tourism, categoryImageView);
                 break;
             case "atm":
                 setupItemIcon(context, R.drawable.ic_local_atm_white, R.color.category_atm, categoryImageView);
                 break;
             case "taxi":
-                setupItemIcon(context, R.drawable.ic_local_transit_station_white, R.color.category_transit, categoryImageView);
+                setupItemIcon(context, R.drawable.ic_local_transit_station_white, R.color.category_transportation, categoryImageView);
                 break;
             case "bus_stop":
-                setupItemIcon(context, R.drawable.ic_local_transit_station_white, R.color.category_transit, categoryImageView);
+                setupItemIcon(context, R.drawable.ic_local_transit_station_white, R.color.category_transportation, categoryImageView);
                 break;
             case "bus_station":
-                setupItemIcon(context, R.drawable.ic_local_transit_station_white, R.color.category_transit, categoryImageView);
+                setupItemIcon(context, R.drawable.ic_local_transit_station_white, R.color.category_transportation, categoryImageView);
                 break;
             case "transportation":
-                setupItemIcon(context, R.drawable.ic_local_transit_station_white, R.color.category_transit, categoryImageView);
+                setupItemIcon(context, R.drawable.ic_local_transit_station_white, R.color.category_transportation, categoryImageView);
                 break;
             case "fuel":
-                setupItemIcon(context, R.drawable.ic_local_gas_station_white, R.color.category_gas, categoryImageView);
+                setupItemIcon(context, R.drawable.ic_local_gas_station_white, R.color.category_gas_station, categoryImageView);
                 break;
             case "hospital":
-                setupItemIcon(context, R.drawable.ic_local_doctor_white, R.color.category_health, categoryImageView);
+                setupItemIcon(context, R.drawable.ic_local_doctor_white, R.color.category_pharmacy, categoryImageView);
                 break;
             case "pharmacy":
-                setupItemIcon(context, R.drawable.ic_local_pharmacy_white, R.color.category_health, categoryImageView);
+                setupItemIcon(context, R.drawable.ic_local_pharmacy_white, R.color.category_pharmacy, categoryImageView);
                 break;
             case "mobile_phone":
-                setupItemIcon(context, R.drawable.ic_phonelink_setup_white, R.color.category_communication, categoryImageView);
+                setupItemIcon(context, R.drawable.ic_phonelink_setup_white, R.color.category_mobile_phone_store, categoryImageView);
                 break;
             case "mobile_shop":
-                setupItemIcon(context, R.drawable.ic_phonelink_setup_white, R.color.category_communication, categoryImageView);
+                setupItemIcon(context, R.drawable.ic_phonelink_setup_white, R.color.category_mobile_phone_store, categoryImageView);
                 break;
             case "fast_food":
                 setupItemIcon(context, R.drawable.ic_room_service_white_24dp, R.color.category_fast_food, categoryImageView);
                 break;
             case "supermarket":
-                setupItemIcon(context, R.drawable.ic_home_white, R.color.category_home, categoryImageView);
+                setupItemIcon(context, R.drawable.ic_store_white_24dp, R.color.category_market, categoryImageView);
                 break;
             default:
                 setupItemIcon(context, R.drawable.ic_map_marker, R.color.category_building, categoryImageView);
